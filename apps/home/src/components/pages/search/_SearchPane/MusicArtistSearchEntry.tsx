c4f6581fcb536b5e19f651f4906485c599e06379
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2024-12-01 20:06:01 +0000

    Music artist search entry tweaks

--- apps/home/src/components/pages/search/_SearchPane/MusicArtistSearchEntry.tsx
+++ apps/home/src/components/pages/search/_SearchPane/MusicArtistSearchEntry.tsx
@@ -13,17 +13,17 @@ export const MusicArtistSearchEntry = (props: MusicArtistSearchEntryProps) => {
 		>
 			<Image
 				src={props.artist?.covers[0].url}
-				class="aspect-square rounded-lg border border-slate-200 transition-all"
+				class="aspect-square rounded-full border border-slate-200 transition-all"
 				alt={props.artist?.name}
 				layout="fullWidth"
 				loading="lazy"
 				{...{ style: { "view-transition-name": props.artist?.slug } }}
 			/>
-			<div>
+			<div class="space-y-1">
 				<h2 class="font-heading text-lg transition-all group-hover:text-blue-500">
 					{props.artist.name}
 				</h2>
-				<p>{props.artist.genres.join(", ")}</p>
+				<p class="text-sm">{props.artist.genres.join(", ")}</p>
 			</div>
 		</a>
 	);
